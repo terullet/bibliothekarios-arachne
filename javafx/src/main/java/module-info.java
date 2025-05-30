31379d664ad44ce98de6f38ebd39e15007e64245
terullet <terullet.467.clystear2@gmail.com>
2025-05-30 21:43:51 +0900

    delete: module-info.java

--- javafx/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module net.terullet.bibliothekarios.arachne.javafx {
-	requires java.desktop;
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.graphics;
-
-	opens net.terullet.bibliothekarios.arachne.javafx to javafx.fxml;
-	exports net.terullet.bibliothekarios.arachne.javafx;
-}
