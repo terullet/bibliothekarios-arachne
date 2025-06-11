9e9ba682396fa82f3ee22ddb048c7ae12b2faaab
terullet <terullet.467.clystear2@gmail.com>
2025-06-11 13:14:58 +0900

    add: work and universe

---
+++ javafx/src/main/resources/db/migration/V1__initialize_database.sql
@@ -0,0 +1,21 @@
+CREATE TABLE universes (
+    universe_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(256) NOT NULL,
+    summary CLOB NOT NULL
+);
+
+CREATE TABLE works (
+    work_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(256) NOT NULL,
+    summary CLOB NOT NULL,
+    registered_at TIMESTAMP(3) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    CONSTRAINT uq_work_site UNIQUE (work_id, site_id),
+    CONSTRAINT fk_work_site FOREIGN KEY site_id REFERENCES sites(site_id) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+
+CREATE TABLE works_universe_map (
+    work_id BIGINT PRIMARY KEY,
+    universe_id BIGINT NOT NULL,
+    CONSTRAINT fk_works_universe_work FOREIGN KEY work_id REFERENCES works(work_id) ON UPDATE RESTRICT ON DELETE CASCADE,
+    CONSTRAINT fk_works_universe_universe FOREIGN KEY universe_id REFERENCES universes(universe_id) ON UPDATE RESTRICT ON DELETE CASCADE
+);
