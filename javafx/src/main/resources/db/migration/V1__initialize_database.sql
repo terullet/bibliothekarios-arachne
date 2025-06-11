c778f4c68189c343b3607e43d5ae49ee5b309dc8
terullet <terullet.467.clystear2@gmail.com>
2025-06-11 13:24:07 +0900

    add: paragraphs

--- javafx/src/main/resources/db/migration/V1__initialize_database.sql
+++ javafx/src/main/resources/db/migration/V1__initialize_database.sql
@@ -73,6 +73,32 @@ CREATE TABLE episode_deletes (
     CONSTRAINT fk_delete_episode FOREIGN KEY episode_id REFERENCES episodes(episode_id) ON UPDATE RESTRICT ON DELETE CASCADE
 );
 
+CREATE TABLE paragraphs (
+    paragraph_id BIGINT PRIMARY KEY,
+    order_number BIGINT NOT NULL,
+    episode_id BIGINT NOT NULL,
+    CONSTRAINT uq_paragraph_episode UNIQUE (paragraph_id, episode_id),
+    CONSTRAINT uq_paragraph_order UNIQUE (episode_id, order_number),
+    CONSTRAINT fk_paragraph_episode FOREIGN KEY episode_id REFERENCES episodes(episode_id)
+);
+
+CREATE TABLE paragraph_updates (
+    update_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    paragraph_id BIGINT NOT NULL,
+    episode_id BIGINT NOT NULL,
+    post_id BIGINT NOT NULL,
+    content CLOB NOT NULL,
+    CONSTRAINT uq_paragraph_episode_post UNIQUE (paragraph_id, post_id),
+    CONSTRAINT fk_paragraph_update_episode_post FOREIGN KEY (post_id, episode_id) REFERENCES episode_posts(post_id, episode_id),
+    CONSTRAINT fk_paragraph_update_paragraph FOREIGN KEY (paragraph_id, episode_id) REFERENCES paragraphs(paragraph_id, episode_id)
+);
+
+CREATE TABLE paragraph_deletes (
+    paragraph_id BIGINT PRIMARY KEY,
+    deleted_at TIMESTAMP(0) WITH TIME ZONE NOT NULL,
+    CONSTRAINT fk_delete_paragraph FOREIGN KEY paragraph_id REFERENCES paragraphs(paragraph_id)
+);
+
 CREATE TABLE contributors (
     contributor_id BIGINT AUTO_INCREMENT PRIMARY KEY,
 );
