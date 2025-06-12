62ca2d5895aef7a8a58ecb21cbdb2f4213c05bc2
terullet <terullet.467.clystear2@gmail.com>
2025-06-12 17:25:06 +0900

    fix: paragraph delete

--- javafx/src/main/resources/db/migration/V1__initialize_database.sql
+++ javafx/src/main/resources/db/migration/V1__initialize_database.sql
@@ -94,7 +94,7 @@ CREATE TABLE paragraphs (
     episode_id BIGINT NOT NULL,
     CONSTRAINT uq_paragraph_episode UNIQUE (paragraph_id, episode_id),
     CONSTRAINT uq_paragraph_order UNIQUE (episode_id, order_number),
-    CONSTRAINT fk_paragraph_episode FOREIGN KEY (episode_id) REFERENCES episodes(episode_id)
+    CONSTRAINT fk_paragraph_episode FOREIGN KEY (episode_id) REFERENCES episodes(episode_id) ON UPDATE RESTRICT ON DELETE CASCADE
 );
 
 CREATE TABLE paragraph_updates (
@@ -104,14 +104,16 @@ CREATE TABLE paragraph_updates (
     post_id BIGINT NOT NULL,
     content CLOB NOT NULL,
     CONSTRAINT uq_paragraph_episode_post UNIQUE (paragraph_id, post_id),
-    CONSTRAINT fk_paragraph_update_episode_post FOREIGN KEY (post_id, episode_id) REFERENCES episode_posts(post_id, episode_id),
-    CONSTRAINT fk_paragraph_update_paragraph FOREIGN KEY (paragraph_id, episode_id) REFERENCES paragraphs(paragraph_id, episode_id)
+    CONSTRAINT fk_paragraph_update_episode_post FOREIGN KEY (post_id, episode_id) REFERENCES episode_posts(post_id, episode_id) ON UPDATE RESTRICT ON DELETE CASCADE,
+    CONSTRAINT fk_paragraph_update_paragraph FOREIGN KEY (paragraph_id, episode_id) REFERENCES paragraphs(paragraph_id, episode_id) ON UPDATE RESTRICT ON DELETE CASCADE
 );
 
 CREATE TABLE paragraph_deletes (
     paragraph_id BIGINT PRIMARY KEY,
-    deleted_at TIMESTAMP(0) WITH TIME ZONE NOT NULL,
-    CONSTRAINT fk_delete_paragraph FOREIGN KEY (paragraph_id) REFERENCES paragraphs(paragraph_id)
+    episode_id BIGINT NOT NULL,
+    post_id BIGINT NOT NULL,
+    CONSTRAINT fk_paragraph_delete_episode_post FOREIGN KEY (episode_id, post_id) REFERENCES episode_posts(post_id, episode_id) ON UPDATE RESTRICT ON DELETE CASCADE,
+    CONSTRAINT fk_paragraph_delete_paragraph FOREIGN KEY (paragraph_id, episode_id) REFERENCES paragraphs(paragraph_id, episode_id) ON UPDATE RESTRICT ON DELETE CASCADE
 );
 
 CREATE TABLE contributors (
