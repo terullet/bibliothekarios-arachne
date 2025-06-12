90e8f3b22b411bc06295180c4b1fed3c74a66c36
terullet <terullet.467.clystear2@gmail.com>
2025-06-12 16:35:33 +0900

    update: contributor, site, hyperlink

--- javafx/src/main/resources/db/migration/V1__initialize_database.sql
+++ javafx/src/main/resources/db/migration/V1__initialize_database.sql
@@ -1,15 +1,8 @@
 CREATE TABLE sites (
-    site_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    site_id INT AUTO_INCREMENT PRIMARY KEY,
     site_name VARCHAR(256) UNIQUE NOT NULL
 );
-
-CREATE TABLE hostnames (
-    host_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    hostname VARCHAR(2048) UNIQUE NOT NULL,
-    site_id BIGINT NOT NULL,
-    description VARCHAR(256) NOT NULL,
-    CONSTRAINT fk_hostname_site FOREIGN KEY (site_id) REFERENCES sites(site_id) ON UPDATE RESTRICT ON DELETE RESTRICT
-);
+INSERT INTO sites(site_name) VALUES ('書籍'), ('小説家になろう'), ('小説家になろう（R18）');
 
 CREATE TABLE universes (
     universe_id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -22,7 +15,6 @@ CREATE TABLE works (
     site_id BIGINT NOT NULL,
     summary CLOB NOT NULL,
     registered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    CONSTRAINT uq_work_site UNIQUE (work_id, site_id),
     CONSTRAINT fk_work_site FOREIGN KEY (site_id) REFERENCES sites(site_id) ON UPDATE RESTRICT ON DELETE RESTRICT
 );
 
@@ -46,6 +38,7 @@ CREATE TABLE sections (
     section_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     work_id BIGINT NOT NULL,
     title VARCHAR(256) NOT NULL,
+    CONSTRAINT uq_section_work UNIQUE (section_id, work_id),
     CONSTRAINT fk_section_work FOREIGN KEY (work_id) REFERENCES works(work_id) ON UPDATE RESTRICT ON DELETE CASCADE
 );
 
@@ -122,33 +115,23 @@ CREATE TABLE paragraph_deletes (
 );
 
 CREATE TABLE contributors (
-    contributor_id BIGINT AUTO_INCREMENT PRIMARY KEY
-);
-
-CREATE TABLE contributors_sites_map (
-    contributor_id BIGINT NOT NULL,
-    site_id BIGINT NOT NULL,
-    handle_name VARCHAR(256) NOT NULL,
-    in_site_id VARCHAR(256) NOT NULL,
-    CONSTRAINT pk_contributor_site PRIMARY KEY (contributor_id, site_id),
-    CONSTRAINT uq_site_in_site UNIQUE (site_id, in_site_id),
-    CONSTRAINT fk_contributors_sites_contributor FOREIGN KEY (contributor_id) REFERENCES contributors(contributor_id) ON UPDATE CASCADE ON DELETE RESTRICT,
-    CONSTRAINT fk_contributors_sites_site FOREIGN KEY (site_id) REFERENCES sites(site_id) ON UPDATE RESTRICT ON DELETE RESTRICT
+    contributor_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    handle_name VARCHAR(256) NOT NULL
 );
 
 CREATE TABLE contribution_types (
     type_id INT AUTO_INCREMENT PRIMARY KEY,
-    description VARCHAR(256) UNIQUE NOT NULL
+    type_name VARCHAR(256) UNIQUE NOT NULL
 );
+INSERT INTO contribution_types (type_name) VALUES ('作者'), ('イラスト'), ('原作'), ('原案');
 
 CREATE TABLE contributions (
     work_id BIGINT NOT NULL,
     contributor_id BIGINT NOT NULL,
-    site_id BIGINT NOT NULL,
     type_id INT NOT NULL,
     CONSTRAINT pk_contribution PRIMARY KEY (work_id, contributor_id, type_id),
-    CONSTRAINT fk_contribution_work_site FOREIGN KEY (work_id, site_id) REFERENCES works(work_id, site_id) ON UPDATE RESTRICT ON DELETE CASCADE,
-    CONSTRAINT fk_contribution_contributor_site FOREIGN KEY (contributor_id, site_id) REFERENCES contributors_sites(contributor_id, site_id) ON UPDATE RESTRICT ON DELETE RESTRICT,
+    CONSTRAINT fk_contribution_work FOREIGN KEY (work_id) REFERENCES works(work_id) ON UPDATE RESTRICT ON DELETE CASCADE,
+    CONSTRAINT fk_contribution_contributor FOREIGN KEY (contributor_id) REFERENCES contributors_sites(contributor_id) ON UPDATE RESTRICT ON DELETE RESTRICT,
     CONSTRAINT fk_contribution_contribution_type FOREIGN KEY (type_id) REFERENCES contribution_types(type_id) ON UPDATE RESTRICT ON DELETE RESTRICT
 );
 
@@ -188,6 +171,7 @@ CREATE TABLE review_target_types (
     type_id INT AUTO_INCREMENT PRIMARY KEY,
     type_name VARCHAR(256) UNIQUE NOT NULL
 );
+INSERT INTO review_target_types (type_name) VALUES ('work'), ('episode'), ('contributor');
 
 CREATE TABLE reviews (
     review_id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -223,6 +207,13 @@ CREATE TABLE review_episodes_map (
     CONSTRAINT fk_reviews_episodes_episode FOREIGN KEY (episode_id) REFERENCES episodes(episode_id) ON UPDATE RESTRICT ON DELETE CASCADE
 );
 
+CREATE TABLE review_contributor_map (
+    review_id BIGINT PRIMARY KEY,
+    contributor_id BIGINT NOT NULL,
+    CONSTRAINT fk_review_contributor_review FOREIGN KEY (review_id) REFERENCES reviews(review_id) ON UPDATE RESTRICT ON DELETE CASCADE,
+    CONSTRAINT fk_review_contributor_contributor FOREIGN KEY (contributor_id) REFERENCES contributors(contributor_id) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+
 CREATE TABLE bookmarkers (
     bookmarker_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     bookmarker_name VARCHAR(256) NOT NULL
@@ -248,4 +239,24 @@ CREATE TABLE reading_record_paragraphs_map (
     CONSTRAINT pk_reading_record_paragraph PRIMARY KEY (record_id, paragraph_id),
     CONSTRAINT fk_reading_record_paragraphs_record FOREIGN KEY (record_id) REFERENCES reading_records(record_id) ON UPDATE RESTRICT ON DELETE CASCADE,
     CONSTRAINT fk_reading_record_paragraphs_paragraph FOREIGN KEY (paragraph_id) REFERENCES paragraphs(paragraph_id) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+
+CREATE TABLE hyperlinks (
+    link_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    link VARCHAR(2048) UNIQUE NOT NULL,
+    link_name VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE hyperlink_work_map (
+    link_id BIGINT PRIMARY KEY,
+    work_id BIGINT NOT NULL,
+    CONSTRAINT fk_hyperlink_work_link FOREIGN KEY (link_id) REFERENCES hyperlinks(link_id),
+    CONSTRAINT fk_hyperlink_work_work FOREIGN KEY (work_id) REFERENCES works(work_id)
+);
+
+CREATE TABLE hyperlink_contributor_map (
+    link_id BIGINT PRIMARY KEY,
+    contributor_id BIGINT NOT NULL,
+    CONSTRAINT fk_hyperlink_contributor_link FOREIGN KEY (link_id) REFERENCES hyperlinks(link_id),
+    CONSTRAINT fk_hyperlink_contributor_contributor FOREIGN KEY (contributor_id) REFERENCES contributors(contributor_id)
 );
