00371635ccace19dfdfb9339356dc18f29f65223
terullet <terullet.467.clystear2@gmail.com>
2025-06-13 09:23:44 +0900

    add: (bookmarker|readingrecord)-episode relationship

--- javafx/src/main/resources/db/migration/V1__initialize_database.sql
+++ javafx/src/main/resources/db/migration/V1__initialize_database.sql
@@ -228,6 +228,13 @@ CREATE TABLE bookmarker_paragraph_map (
     CONSTRAINT fk_bookmarker_paragraphs_paragraph FOREIGN KEY (paragraph_id) REFERENCES paragraphs(paragraph_id) ON UPDATE RESTRICT ON DELETE RESTRICT
 );
 
+CREATE TABLE bookmarker_episode_map (
+    bookmarker_id BIGINT PRIMARY KEY,
+    episode_id BIGINT NOT NULL,
+    CONSTRAINT fk_bookmarker_episode_bookmarker FOREIGN KEY (bookmarker_id) REFERENCES bookmarkers(bookmarker_id) ON UPDATE RESTRICT ON DELETE CASCADE,
+    CONSTRAINT fk_bookmarker_episode_episode FOREIGN KEY (episode_id) REFERENCES episodes(episode_id) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+
 CREATE TABLE reading_records (
     record_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     read_seconds INT NOT NULL,
@@ -241,6 +248,14 @@ CREATE TABLE reading_record_paragraphs_map (
     CONSTRAINT pk_reading_record_paragraph PRIMARY KEY (record_id, paragraph_id),
     CONSTRAINT fk_reading_record_paragraphs_record FOREIGN KEY (record_id) REFERENCES reading_records(record_id) ON UPDATE RESTRICT ON DELETE CASCADE,
     CONSTRAINT fk_reading_record_paragraphs_paragraph FOREIGN KEY (paragraph_id) REFERENCES paragraphs(paragraph_id) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+
+CREATE TABLE reading_record_episode_map (
+    record_id BIGINT NOT NULL,
+    episode_id BIGINT NOT NULL,
+    CONSTRAINT pk_reading_record_episode PRIMARY KEY (record_id, episode_id),
+    CONSTRAINT fk_reading_record_episode_record FOREIGN KEY (record_id) REFERENCES reading_records(record_id) ON UPDATE RESTRICT ON DELETE CASCADE,
+    CONSTRAINT fk_reading_record_episode_episode FOREIGN KEY (episode_id) REFERENCES episodes(episode_id) ON UPDATE RESTRICT ON DELETE RESTRICT
 );
 
 CREATE TABLE hyperlinks (
