c0fc5f01640d9ac8aaaae1a11577a8ebb302337a
terullet <terullet.467.clystear2@gmail.com>
2025-06-18 16:24:46 +0900

    update: universe entity

--- common/src/main/java/net/terullet/bibliothekarios/arachne/repository/entity/UniverseEntity.java
+++ common/src/main/java/net/terullet/bibliothekarios/arachne/repository/entity/UniverseEntity.java
@@ -1,14 +1,19 @@
 package net.terullet.bibliothekarios.arachne.repository.entity;
 
+import java.util.Collections;
+import java.util.List;
+
 public final class UniverseEntity {
 	private final long id;
 	private final String title;
 	private final String summary;
+	private final List<WorkOverviewEntity> works;
 
-	public UniverseEntity(long id, String title, String summary) {
+	public UniverseEntity(long id, String title, String summary, List<WorkOverviewEntity> works) {
 		this.id = id;
 		this.title = title;
 		this.summary = summary;
+		this.works = Collections.unmodifiableList(works);
 	}
 
 	public long getId() {
@@ -21,5 +26,9 @@ public final class UniverseEntity {
 
 	public String getSummary() {
 		return this.summary;
+	}
+
+	public List<WorkOverviewEntity> getWorks() {
+		return this.works;
 	}
 }
