a43864f5e1fc9babbd0a4b607b6f67f57aa9fa4e
terullet <terullet.467.clystear2@gmail.com>
2025-06-18 16:20:44 +0900

    update: mapper interface to use overview entities

--- common/src/main/java/net/terullet/bibliothekarios/arachne/repository/mapper/UniverseMapper.java
+++ common/src/main/java/net/terullet/bibliothekarios/arachne/repository/mapper/UniverseMapper.java
@@ -2,11 +2,13 @@ package net.terullet.bibliothekarios.arachne.repository.mapper;
 
 import net.terullet.bibliothekarios.arachne.repository.entity.UniverseCreationRequest;
 import net.terullet.bibliothekarios.arachne.repository.entity.UniverseEntity;
+import net.terullet.bibliothekarios.arachne.repository.entity.UniverseOverviewEntity;
 import net.terullet.bibliothekarios.arachne.repository.entity.UniverseUpdateRequest;
 
 public interface UniverseMapper {
 	UniverseEntity selectUniverseById(long id);
-	int insertUniverse(UniverseCreationRequest workUniverse);
+	UniverseOverviewEntity selectUniverseByWorkId(long workId);
+	int insertUniverse(UniverseCreationRequest universe);
 	int updateUniverse(UniverseUpdateRequest universe);
 	int deleteUniverse(long id);
 }
--- common/src/main/java/net/terullet/bibliothekarios/arachne/repository/mapper/WorkMapper.java
+++ common/src/main/java/net/terullet/bibliothekarios/arachne/repository/mapper/WorkMapper.java
@@ -1,15 +1,15 @@
 package net.terullet.bibliothekarios.arachne.repository.mapper;
 
 import net.terullet.bibliothekarios.arachne.repository.entity.WorkEntity;
-import net.terullet.bibliothekarios.arachne.repository.entity.WorkListElementEntity;
+import net.terullet.bibliothekarios.arachne.repository.entity.WorkOverviewEntity;
 import net.terullet.bibliothekarios.arachne.repository.entity.WorkTitleUpdateRequest;
 
 import java.util.List;
 
 public interface WorkMapper {
-	List<WorkListElementEntity> selectAllWorks();
-	List<WorkListElementEntity> selectWorksByUniverseId(long universeId);
-	WorkEntity selectWorkById(long id);
+	List<WorkOverviewEntity> selectAllWorks();
+	List<WorkOverviewEntity> selectWorksByUniverseId(long universeId);
+	WorkEntity.Factory selectWorkById(long id);
 	int updateWorkTitle(WorkTitleUpdateRequest work);
 	int deleteWork(long id);
 }
