db72bd4c121d37db7da2984b7cfbc716e46c3fcf
terullet <terullet.467.clystear2@gmail.com>
2025-06-23 16:25:52 +0900

    remove: ui interfaces

--- common/src/main/java/net/terullet/bibliothekarios/arachne/ui/Contribution.java
+++
@@ -1,7 +0,0 @@
-package net.terullet.bibliothekarios.arachne.ui;
-
-public interface Contribution {
-	long getId();
-	Work getWork();
-	Contributor getContributor();
-}
--- common/src/main/java/net/terullet/bibliothekarios/arachne/ui/web/WebWork.java
+++
@@ -1,6 +0,0 @@
-package net.terullet.bibliothekarios.arachne.ui.web;
-
-import net.terullet.bibliothekarios.arachne.ui.Work;
-
-public interface WebWork extends Work {
-}
