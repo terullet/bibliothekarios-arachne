4e5e4f1bc9e2e250b930c7ba11cc4779269628bb
terullet <terullet.467.clystear2@gmail.com>
2025-06-25 14:27:39 +0900

    update: exclude params in WorkCreationRequestEntity from NarouWorkCreationRequestEntity.

--- common/src/main/java/net/terullet/bibliothekarios/arachne/repository/entity/web/com/syosetu/NarouWorkCreationRequestEntity.java
+++ common/src/main/java/net/terullet/bibliothekarios/arachne/repository/entity/web/com/syosetu/NarouWorkCreationRequestEntity.java
@@ -6,20 +6,14 @@ import net.terullet.bibliothekarios.arachne.domain.web.com.syosetu.NarouWorkType
 import java.time.LocalDateTime;
 
 public final class NarouWorkCreationRequestEntity {
-	private Long id;
+	private final long workId;
 	private final String ncode;
 	private final long narouId;
 	private final NarouWorkType workType;
 	private final NarouGenre genre;
-	private final String summary;
-	private LocalDateTime registeredAt;
 
-	public Long getId() {
-		return this.id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
+	public long getWorkId() {
+		return this.workId;
 	}
 
 	public String getNcode() {
@@ -38,23 +32,11 @@ public final class NarouWorkCreationRequestEntity {
 		return this.genre;
 	}
 
-	public String getSummary() {
-		return this.summary;
-	}
-
-	public LocalDateTime getRegisteredAt() {
-		return this.registeredAt;
-	}
-
-	public void setRegisteredAt(LocalDateTime registeredAt) {
-		this.registeredAt = registeredAt;
-	}
-
-	public NarouWorkCreationRequestEntity(String ncode, long narouId, NarouWorkType workType, NarouGenre genre, String summary) {
+	public NarouWorkCreationRequestEntity(long workId, String ncode, long narouId, NarouWorkType workType, NarouGenre genre) {
+		this.workId = workId;
 		this.ncode = ncode;
 		this.narouId = narouId;
 		this.workType = workType;
 		this.genre = genre;
-		this.summary = summary;
 	}
 }
